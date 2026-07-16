7882b45121809b198f409f388018cd0db633a306
yanseim <51874757+yanseim@users.noreply.github.com>
2026-07-16 16:31:19 +0800

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,25 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        S = []
+        for index,num in enumerate(nums):# 注意enu语法！先index再内容！
+            S.append([num,index])
+        
+        S.sort()
+
+        i,j = 0,len(nums)-1
+        # while i<j:
+        #     if S[i][0]+S[j][0]<target:
+        #         j=j-1
+        #     elif S[i][0]+S[j][0]>target:
+        # 这部分我一直在想，如果j减下来减多了发现小于了怎么办？后来明白如果sorted就不可能会减多
+        while i<j:
+            cur = S[i][0]+S[j][0]
+            # print(cur)
+            if cur==target:
+                return [min(S[i][1],S[j][1]),max(S[i][1],S[j][1])]
+            elif cur<target:
+                i = i+1
+            else:
+                j = j-1
+
+        return []
